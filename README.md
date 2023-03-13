1871db8a60effa9bfc321d57600a87910fc6944e
Marz Rover <marz@marzvrover.com>
2023-03-12 22:17:19 -0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # mktmp
-Make tempfile that is cleaned up by tmpreaper
+
+Make a tempfile that is cleaned up by tmpreaper using crontab to run tmpreaper every minute.
+
+I tried to make this as POSIX compliant as possible with the only additional requirement being tmpreaper.
+
+tmpreaper comes is the Debian package tmpreaper. It is widely available on many operating systems including macOS via [homebrew](https://brew.sh) and [macports](https://www.macports.org/).
+
+## Purpose
+
+I wanted to create an easy way to help people stay in compliance with data retention policies. This is a simple wrapper around mktemp that creates a file in a directory that is cleaned up by tmpreaper every minute. It is still recommended to manually clean up the files after you are done with them, but this provides an addtional layer of protection.
+
+## Usage
+
+```bash
+mktmp <tmpreaper lifetime> [mktemp arguments]
+```
+
+The files are created in `/tmp/tmpreaper.$LIFETIME` and are cleaned up by tmpreaper every minute according to the lifetime.
+
+## Thanks
+
+- [tmpreaper](https://packages.debian.org/source/bullseye/tmpreaper) by [Debian](https://www.debian.org/)
---
+++ mktmp.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+# designed to work on POSIX systems that can run
+
+function guardDependencies {
+    hasDependencies=true
+
+    # check which is installed
+    if ! command -v which &> /dev/null
+    then
+        >&2 echo "which is not installed. Please install it."
+        $hasDependencies=false
+    fi
+
+    # check if mktemp is installed
+    if ! command -v mktemp &> /dev/null
+    then
+        >&2 echo "mktemp is not installed. Please install it."
+        $hasDependencies=false
+    fi
+
+    # first check that tmpreaper is installed
+    if ! command -v tmpreaper &> /dev/null
+    then
+        >&2 echo "tmpreaper is not installed. Please install it first."
+        $hasDependencies=false
+    fi
+
+    # check if hasDependencies is false
+    if [ $hasDependencies = false ]
+    then
+        exit 1
+    fi
+}
+
+guardDependencies
+
+LIFETIME=$1; shift # pop the lifetime argument
+TMPDIR="/tmp/tmpreaper.$LIFETIME"
+
+# guard against no arguments
+if [ $# -eq 0 ]
+then
+    >&2 echo "No arguments supplied. Please supply a lifetime for the temporary file"
+    exit 1
+fi
+
+# check that the $TMPDIR directory exists
+if [ ! -d "$TMPDIR" ]
+then
+    mkdir -p "$TMPDIR"
+fi
+
+# set tmpreaper to run every minute using crontab
+if ! crontab -l | grep -q "$TMPDIR"
+then
+    (crontab -l ; echo "* * * * * `which tmpreaper` $LIFETIME $TMPDIR") | crontab -
+fi
+
+# create a temporary file passing in the rest of the arguments
+mktemp $@ "${TMPDIR}/XXXXXXXXXXXX"
